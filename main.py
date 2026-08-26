a95ca8cc2685f979a473ac7d1a5b5351e8f16da2
Singh-9E-ay2526 <singh.ta@obmontessori.edu.ph>
2026-08-26 22:20:57 +0800

    Change number input type to float and update display

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ from pyscript import display, document
 
 def adding_numbers(e):
 
-    number1 = int(document.getElementById('num1').value)
-    number2 = int(document.getElementById('num2').value)
+    number1 = float(document.getElementById("num1").value)
+    number2 = float(document.getElementById("num2").value)
 
     add = number1 + number2
     sub = number1 - number2
     mul = number1 * number2
     div = number1 / number2
 
-    document.getElementById('result').innerHTML = ""
+    document.getElementById("result").innerHTML = ""
 
     display(f"""
     <h3>Results</h3>
 
-    Addition: {add}<br>
-    Subtraction: {sub}<br>
-    Multiplication: {mul}<br>
-    Division: {div}
-    """, target='result')
+    ➕ Addition: {add}<br>
+    ➖ Subtraction: {sub}<br>
+    ✖️ Multiplication: {mul}<br>
+    ➗ Division: {div}
+    """, target="result")
